f26ea90f0411ba08027c0cf6e35e8b7ca2ac82a4
Matthias Wirth <matthias.wirth@gmail.com>
2019-12-28 15:16:42 +0100

    add option to disable TIS-B

--- convert.sh
+++ convert.sh
@@ -6,7 +6,7 @@ trap "kill 0" EXIT
 while sleep 1
 do
 	sleep 5 &
-	socat -u "TCP:$SOURCE,keepalive,keepidle=30,keepintvl=30,keepcnt=2,connect-timeout=10,forever,interval=15" STDOUT | "$IPATH/uat2esnt" | socat -u STDIN "TCP:localhost:$AVR_IN_PORT"
+	socat -u "TCP:$SOURCE,keepalive,keepidle=30,keepintvl=30,keepcnt=2,connect-timeout=10,forever,interval=15" STDOUT | "$IPATH/uat2esnt" $CONVERT_OPTIONS | socat -u STDIN "TCP:localhost:$AVR_IN_PORT"
 	wait
 done &
 
