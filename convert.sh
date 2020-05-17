03225d20d3bf116b8627627af06e0b1a91ce7b64
Matthias Wirth <matthias.wirth@gmail.com>
2020-05-17 17:01:27 +0200

    update convert.sh

--- convert.sh
+++ convert.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
 
-trap "exit" INT TERM
-trap "kill 0" EXIT
-
+SO_OPTIONS="keepalive,keepidle=30,keepintvl=30,keepcnt=2,connect-timeout=10,forever,interval=15"
 while sleep 1
 do
 	sleep 5 &
-	socat -u "TCP:$SOURCE,keepalive,keepidle=30,keepintvl=30,keepcnt=2,connect-timeout=10,forever,interval=15" STDOUT | "$IPATH/uat2esnt" $CONVERT_OPTIONS | socat -u STDIN "TCP:localhost:$AVR_IN_PORT"
+	socat -d -d -u "TCP:$SOURCE,$SO_OPTIONS" STDOUT | "$IPATH/uat2esnt" $CONVERT_OPTIONS \
+        | socat -d -d -u STDIN "TCP:localhost:$AVR_IN_PORT,$SO_OPTIONS"
 	wait
-done &
-
-
-wait
+done
