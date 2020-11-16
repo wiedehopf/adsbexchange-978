18171ffee6ddb83722ca0172fea4c715665c811c
Matthias Wirth <matthias.wirth@gmail.com>
2020-11-16 10:21:30 +0100

    don't put data into adsbexchange-feed anymore.

--- convert.sh
+++ convert.sh
@@ -4,5 +4,5 @@ SO_OPTIONS="keepalive,keepidle=30,keepintvl=30,keepcnt=2"
 while sleep 5
 do
 	socat -d -u "TCP:$SOURCE,$SO_OPTIONS" STDOUT | "$IPATH/uat2esnt" $CONVERT_OPTIONS \
-        | socat -d -u STDIN "TCP:localhost:$AVR_IN_PORT,$SO_OPTIONS"
+        | socat -d -u STDIN "TCP:localhost:37981,$SO_OPTIONS"
 done
